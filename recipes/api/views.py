31a3628058b1025cf00d0b632fb3458f000eddaf
Harrison Stein <harrison.m.stein@gmail.com>
2024-07-08 16:54:38 -0400

    apply isauthorreadonly permissions

--- recipes/api/views.py
+++ recipes/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import viewsets
 
 from data.models import Recipe
@@ -19,4 +19,4 @@ class RecipeViewSet(viewsets.ModelViewSet):
    queryset = Recipe.objects.all()
    serializer_class = RecipeSerializer
    authentication_classes = (TokenAuthentication, )
-   permission_classes = (IsAuthenticated, )
+   permission_classes = (IsAuthenticatedOrReadOnly, )
